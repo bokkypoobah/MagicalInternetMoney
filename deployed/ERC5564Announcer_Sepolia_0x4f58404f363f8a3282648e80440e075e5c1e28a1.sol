d6ef01af775363e4165d93a13d63b495832baa25
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-12-25 12:35:00 +1100

    ERC5564Announcer deployed on Sepolia

---
+++ deployed/ERC5564Announcer_Sepolia_0x4f58404f363f8a3282648e80440e075e5c1e28a1.sol
@@ -0,0 +1,47 @@
+/**
+ *Submitted for verification at Etherscan.io on 2023-12-25
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// From https://eips.ethereum.org/EIPS/eip-5564
+
+/// @notice Interface for announcing when something is sent to a stealth address.
+interface IERC5564Announcer {
+  /// @dev Emitted when sending something to a stealth address.
+  /// @dev See the `announce` method for documentation on the parameters.
+  event Announcement(uint256 indexed schemeId, address indexed stealthAddress, address indexed caller, bytes ephemeralPubKey, bytes metadata);
+
+  /// @dev Called by integrators to emit an `Announcement` event.
+  /// @param schemeId The integer specifying the applied stealth address scheme.
+  /// @param stealthAddress The computed stealth address for the recipient.
+  /// @param ephemeralPubKey Ephemeral public key used by the sender.
+  /// @param metadata An arbitrary field MUST include the view tag in the first byte.
+  /// Besides the view tag, the metadata can be used by the senders however they like,
+  /// but the below guidelines are recommended:
+  /// The first byte of the metadata MUST be the view tag.
+  /// - When sending/interacting with the native token of the blockchain (cf. ETH), the metadata SHOULD be structured as follows:
+  ///     - Byte 1 MUST be the view tag, as specified above.
+  ///     - Bytes 2-5 are `0xeeeeeeee`
+  ///     - Bytes 6-25 are the address 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE.
+  ///     - Bytes 26-57 are the amount of ETH being sent.
+  /// - When interacting with ERC-20/ERC-721/etc. tokens, the metadata SHOULD be structured as follows:
+  ///   - Byte 1 MUST be the view tag, as specified above.
+  ///   - Bytes 2-5 are a function identifier. When a function selector (e.g.
+  ///     the first (left, high-order in big-endian) four bytes of the Keccak-256
+  ///     hash of the signature of the function, like Solidity and Vyper use) is
+  ///     available, it MUST be used.
+  ///   - Bytes 6-25 are the token contract address.
+  ///   - Bytes 26-57 are the amount of tokens being sent/interacted with for fungible tokens, or
+  ///     the token ID for non-fungible tokens.
+  function announce(uint256 schemeId, address stealthAddress, bytes memory ephemeralPubKey, bytes memory metadata) external;
+}
+
+
+/// @notice Announcing when something is sent to a stealth address.
+contract ERC5564Announcer is IERC5564Announcer {
+  function announce(uint256 schemeId, address stealthAddress, bytes memory ephemeralPubKey, bytes memory metadata) external {
+    emit Announcement(schemeId, stealthAddress, msg.sender, ephemeralPubKey, metadata);
+  }
+}
