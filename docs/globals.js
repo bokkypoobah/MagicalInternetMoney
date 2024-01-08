6bb57ab676f11a3fddb34d9f2c40ba5bc7b40212
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-01-09 07:57:07 +1100

    big tidy

--- docs/globals.js
+++ docs/globals.js
@@ -1,4 +1,5 @@
 const ADDRESS0 = "0x0000000000000000000000000000000000000000";
+const ADDRESS_ETHEREUMS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 const SECP256K1_N = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141";
 const generateRange = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 const delay = ms => new Promise(res => setTimeout(res, ms));
