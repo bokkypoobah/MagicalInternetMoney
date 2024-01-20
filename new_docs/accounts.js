3f4cf0bd802ea24bcb3e5fb326b8688c48dad827
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-01-21 08:41:37 +1100

    wip

--- new_docs/accounts.js
+++ new_docs/accounts.js
@@ -46,7 +46,7 @@ const Accounts = {
             </b-form-group>
 
             <b-form-group v-if="newAccount.action == 'addAddress' || newAccount.action == 'addStealthMetaAddress'" label="Mine:" label-for="addnewaccount-mine" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-              <b-button size="sm" :pressed.sync="newAccount.mine" @click="saveSettings" variant="transparent"><b-icon :icon="newAccount.mine ? 'star-fill' : 'star'" shift-v="+1" font-scale="0.95" :variant="newAccount.mine ? 'warning' : 'secondary'"></b-icon></b-button>
+              <b-button size="sm" id="addnewaccount-mine" :pressed.sync="newAccount.mine" @click="saveSettings" variant="transparent"><b-icon :icon="newAccount.mine ? 'star-fill' : 'star'" shift-v="+1" font-scale="0.95" :variant="newAccount.mine ? 'warning' : 'secondary'"></b-icon></b-button>
               <!-- <b-form-checkbox size="sm" id="addnewaccount-mine" v-model.trim="newAccount.mine" @change="saveSettings" class="mt-1 ml-2"></b-form-checkbox> -->
             </b-form-group>
 
@@ -64,6 +64,38 @@ const Accounts = {
 
         <b-modal ref="modalaccount" id="modal-account" hide-footer body-bg-variant="light" size="lg">
           <template #modal-title>Account</template>
+
+          <b-form-group label="Address:" label-for="account-address" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+            <b-form-input size="sm" readonly id="account-address" v-model.trim="account.account" class="w-75"></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Source:" label-for="account-source" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+            <b-form-input size="sm" readonly id="account-source" :value="account.source && (account.source.substring(0, 1).toUpperCase() + account.source.slice(1))" class="w-25"></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Mine:" label-for="account-mine" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+            <b-button size="sm" id="account-mine" :pressed.sync="account.mine" @click="saveSettings" variant="transparent"><b-icon :icon="account.mine ? 'star-fill' : 'star'" shift-v="+1" font-scale="0.95" :variant="account.mine ? 'warning' : 'secondary'"></b-icon></b-button>
+            <!-- <b-form-checkbox size="sm" id="addnewaccount-mine" v-model.trim="newAccount.mine" @change="saveSettings" class="mt-1 ml-2"></b-form-checkbox> -->
+          </b-form-group>
+
+          <b-form-group label="Favourite:" label-for="account-favourite" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+            <b-button size="sm" id="account-favourite" :pressed.sync="account.favourite" @click="saveSettings" variant="transparent"><b-icon :icon="account.favourite ? 'heart-fill' : 'heart'" shift-v="+1" font-scale="0.95" variant="danger"></b-icon></b-button>
+            <!-- <b-form-checkbox size="sm" id="addnewaccount-mine" v-model.trim="newAccount.mine" @change="saveSettings" class="mt-1 ml-2"></b-form-checkbox> -->
+          </b-form-group>
+
+          <b-form-group label="ENS Name:" label-for="account-ensname" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+            <b-form-input size="sm" id="account-ensname" v-model.trim="account.ensName" class="w-75"></b-form-input>
+          </b-form-group>
+
+          <!-- this.account.account = item[0].account;
+          this.account.type = item[0].type;
+          this.account.source = item[0].source;
+          this.account.mine = item[0].mine;
+          this.account.favourite = item[0].favourite;
+          this.account.ensName = item[0].ensName;
+          this.account.name = item[0].name;
+          this.account.notes = item[0].notes; -->
+
           <font size="-1">
             <pre>
 {{ account }}
@@ -73,6 +105,20 @@ const Accounts = {
 
         <b-modal ref="modalstealthmetaccount" id="modal-stealthmetaccount" hide-footer body-bg-variant="light" size="lg">
           <template #modal-title>Stealth Meta-Address</template>
+
+          <!-- this.stealthMetaAccount.account = item[0].account;
+          this.stealthMetaAccount.type = item[0].type;
+          this.stealthMetaAccount.linkedToAddress = item[0].linkedToAddress;
+          this.stealthMetaAccount.phrase = item[0].phrase;
+          this.stealthMetaAccount.spendingPrivateKey = null;
+          this.stealthMetaAccount.viewingPrivateKey = item[0].viewingPrivateKey;
+          this.stealthMetaAccount.viewingPublicKey = item[0].viewingPublicKey;
+          this.stealthMetaAccount.source = item[0].source;
+          this.stealthMetaAccount.mine = item[0].mine;
+          this.stealthMetaAccount.favourite = item[0].favourite;
+          this.stealthMetaAccount.name = item[0].name;
+          this.stealthMetaAccount.notes = item[0].notes; -->
+
           <font size="-1">
             <pre>
 {{ stealthMetaAccount }}
