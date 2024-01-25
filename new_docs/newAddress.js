53665c71d2dccb2f169a33162f158515630d0959
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-01-25 11:34:56 +1100

    new address modal wip

--- new_docs/newAddress.js
+++ new_docs/newAddress.js
@@ -1,11 +1,8 @@
 const NewAddress = {
   template: `
     <div>
-      aaaa
-
       <b-modal ref="viewaddress" v-model="show" id="modal-newaddress" hide-footer header-class="m-0 px-3 py-2" body-bg-variant="light" size="lg">
         <template #modal-title>New Address</template>
-
         <b-form-group label="Action: " label-for="addnewaddress-type" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
           <b-form-select size="sm" id="addnewaddress-type" v-model="action" :options="newAccountActions" class="w-50"></b-form-select>
         </b-form-group>
@@ -15,77 +12,37 @@ const NewAddress = {
         <b-form-group v-if="action == 'generateStealthMetaAddress'" label="" label-for="addnewaddress-generate" label-size="sm" label-cols-sm="3" label-align-sm="right" description="Click Generate and sign the phrase with your web3 attached account" class="mx-0 my-1 p-0">
           <b-button size="sm" :disabled="!coinbase" id="addnewaddress-generate" @click="generateNewStealthMetaAddress" variant="primary">Generate</b-button>
         </b-form-group>
-        <!-- <b-form-group v-if="action == 'addCoinbase'" label="Attached Web3 Address:" label-for="addnewaddress-coinbase" label-size="sm" label-cols-sm="3" label-align-sm="right" :state="addNewAddressCoinbaseFeedback == null" :invalid-feedback="addNewAddressCoinbaseFeedback" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" readonly id="addnewaddress-coinbase" :value="coinbase" class="w-75"></b-form-input>
-        </b-form-group> -->
-        <!-- <b-form-group v-if="action == 'addAddress'" label="Address:" label-for="addnewaddress-address" label-size="sm" label-cols-sm="3" label-align-sm="right" :state="!address || addNewAddressAddressFeedback == null" :invalid-feedback="addNewAddressAddressFeedback" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" id="addnewaddress-address" v-model.trim="address" placeholder="0x1234...6789" class="w-75"></b-form-input>
-        </b-form-group> -->
-
-        {{ action }}
+        <b-form-group v-if="action == 'addCoinbase'" label="Attached Web3 Address:" label-for="addnewaddress-coinbase" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-form-input size="sm" plaintext id="addnewaddress-coinbase" :value="coinbase" class="px-2 w-75"></b-form-input>
+        </b-form-group>
+        <b-form-group v-if="action == 'addAddress'" label="Address:" label-for="addnewaddress-address" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-form-input size="sm" id="addnewaddress-address" v-model.trim="address" debounce="600" placeholder="0x1234...6789" class="w-75"></b-form-input>
+        </b-form-group>
         <b-form-group v-if="action == 'addStealthMetaAddress'" label="Stealth Meta-Address:" label-for="addnewaddress-stealthmetaaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
           <b-form-textarea size="sm" id="addnewaddress-stealthmetaaddress" v-model.trim="address" debounce="600" placeholder="st:eth:0x1234...6789" rows="3" max-rows="4" class="w-100"></b-form-textarea>
         </b-form-group>
-
-
-        <!-- <b-form-group v-if="action == 'addStealthMetaAddress'" label="Linked To Address:" label-for="addnewaddress-linkedtoaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" :state="!linkedToAddress || addNewAddressLinkedToFeedback == null" :invalid-feedback="addNewAddressLinkedToFeedback" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" id="addnewaddress-linkedtoaddress" v-model.trim="linkedToAddress" placeholder="0x1234...6789" class="w-75"></b-form-input>
-        </b-form-group> -->
-        <!-- <b-form-group v-if="action == 'generateStealthMetaAddress'" label="Generated Stealth Meta-Address:" label-for="addnewaddress-generatedstealthmetaaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-form-textarea size="sm" readonly id="addnewaddress-generatedstealthmetaaddress" v-model.trim="stealthMetaAddress" placeholder="Click generate and sign the phrase with your web3 attached wallet" rows="3" max-rows="4" class="w-75"></b-form-textarea>
-        </b-form-group> -->
-        <!-- <b-form-group v-if="action == 'generateStealthMetaAddress'" label="Linked To Address:" label-for="addnewaddress-generatedlinkedtoaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" description="Attached web3 address" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" readonly id="addnewaddress-coinbase" :value="linkedToAddress" class="w-75"></b-form-input>
-        </b-form-group> -->
-        <!-- <b-form-group v-if="action == 'addAddress' || action == 'addStealthMetaAddress'" label="Mine:" label-for="addnewaddress-mine" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-button size="sm" id="addnewaddress-mine" :pressed.sync="mine" @click="saveSettings" variant="transparent"><b-icon :icon="mine ? 'star-fill' : 'star'" shift-v="+1" font-scale="0.95" :variant="mine ? 'warning' : 'secondary'"></b-icon></b-button>
-        </b-form-group> -->
-        <!-- <b-form-group label="Favourite:" label-for="addnewaddress-favourite" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-button size="sm" id="addnewaddress-favourite" :pressed.sync="favourite" @click="saveSettings" variant="transparent"><b-icon :icon="favourite ? 'heart-fill' : 'heart'" shift-v="+1" font-scale="0.95" variant="danger"></b-icon></b-button>
-        </b-form-group> -->
-        <!-- <b-form-group label="Name:" label-for="addnewaddress-name" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" id="addnewaddress-name" v-model.trim="name" @change="saveSettings" placeholder="optional" class="w-50"></b-form-input>
-        </b-form-group> -->
-        <!-- <b-form-group label="" label-for="addnewaddress-submit" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-button size="sm" :disabled="!!addNewAddressFeedback" id="addnewaddress-submit" @click="addNewAddress" variant="primary">Add/Update</b-button>
-        </b-form-group> -->
-
-
-        <!--
-        <b-form-group label="Address:" label-for="address-address" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-input-group size="sm" class="w-100">
-            <b-form-input size="sm" plaintext id="address-address" v-model.trim="address" class="px-2"></b-form-input>
-            <b-input-group-append>
-              <div>
-                <b-button size="sm" :href="'https://sepolia.etherscan.io/address/' + address" variant="link" v-b-popover.hover="'View in explorer'" target="_blank" class="m-0 ml-1 p-0"><b-icon-link45deg shift-v="+1" font-scale="0.95"></b-icon-link45deg></b-button>
-              </div>
-            </b-input-group-append>
-          </b-input-group>
+        <b-form-group v-if="action == 'addStealthMetaAddress'" label="Linked To Address:" label-for="addnewaddress-linkedtoaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-form-input size="sm" id="addnewaddress-linkedtoaddress" v-model.trim="linkedToAddress" debounce="600" placeholder="0x1234...6789" class="w-75"></b-form-input>
         </b-form-group>
-        <b-form-group v-if="false" label="ENS Name:" label-for="address-ensname" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" plaintext id="address-ensname" v-model.trim="account.ensName" class="px-2 w-75"></b-form-input>
+        <b-form-group v-if="action == 'generateStealthMetaAddress' && address" label="Generated Stealth Meta-Address:" label-for="addnewaddress-generatedstealthmetaaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-form-textarea size="sm" plaintext id="addnewaddress-generatedstealthmetaaddress" v-model.trim="address" placeholder="Click generate and sign the phrase with your web3 attached wallet" rows="3" max-rows="4" class="px-2 w-75"></b-form-textarea>
         </b-form-group>
-        <b-form-group label="Name:" label-for="address-name" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-input-group size="sm" class="w-75">
-            <b-form-input size="sm" type="text" id="address-name" v-model.trim="name" debounce="600" placeholder="optional"></b-form-input>
-            <b-input-group-append>
-              <div>
-                <b-button size="sm" :pressed.sync="mine" variant="transparent" v-b-popover.hover="addressTypeInfo[type || 'address'].name" class="m-0 ml-5 p-0"><b-icon :icon="mine ? 'star-fill' : 'star'" shift-v="+1" font-scale="0.95" :variant="addressTypeInfo[type || 'address'].variant"></b-icon></b-button>
-                <b-button size="sm" :pressed.sync="favourite" variant="transparent" v-b-popover.hover="'Favourite?'" class="m-0 ml-1 p-0"><b-icon :icon="favourite ? 'heart-fill' : 'heart'" shift-v="+1" font-scale="0.95" variant="danger"></b-icon></b-button>
-              </div>
-            </b-input-group-append>
-          </b-input-group>
+        <b-form-group v-if="action == 'generateStealthMetaAddress' && linkedToAddress" label="Linked To Address:" label-for="addnewaddress-generatedlinkedtoaddress" label-size="sm" label-cols-sm="3" label-align-sm="right" description="Attached web3 address" class="mx-0 my-1 p-0">
+          <b-form-input size="sm" plaintext id="addnewaddress-coinbase" :value="linkedToAddress" class="px-2 w-75"></b-form-input>
         </b-form-group>
-        <b-form-group label="Notes:" label-for="address-notes" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-form-textarea size="sm" id="address-notes" v-model.trim="notes" debounce="600" placeholder="..." class="w-100"></b-form-textarea>
+        <b-form-group v-if="action == 'addAddress' || action == 'addStealthMetaAddress'" label="Mine:" label-for="addnewaddress-mine" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-button size="sm" id="addnewaddress-mine" :pressed.sync="mine" variant="transparent"><b-icon :icon="mine ? 'star-fill' : 'star'" shift-v="+1" font-scale="0.95" :variant="mine ? 'warning' : 'secondary'"></b-icon></b-button>
         </b-form-group>
-        <b-form-group label="Source:" label-for="address-source" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-form-input size="sm" plaintext id="address-source" :value="source && (source.substring(0, 1).toUpperCase() + source.slice(1))" class="px-2 w-25"></b-form-input>
+        <b-form-group label="Favourite:" label-for="addnewaddress-favourite" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-button size="sm" id="addnewaddress-favourite" :pressed.sync="favourite" variant="transparent"><b-icon :icon="favourite ? 'heart-fill' : 'heart'" shift-v="+1" font-scale="0.95" variant="danger"></b-icon></b-button>
         </b-form-group>
-        <b-form-group v-if="address" label="" label-for="address-delete" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-          <b-button size="sm" @click="deleteAddress(address);" variant="link" v-b-popover.hover.top="'Delete address ' + address.substring(0, 10) + '...' + address.slice(-8) + '?'"><b-icon-trash shift-v="+1" font-scale="1.1" variant="danger"></b-icon-trash></b-button>
+        <b-form-group label="Name:" label-for="addnewaddress-name" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <b-form-input size="sm" id="addnewaddress-name" v-model.trim="name" debounce="600" placeholder="optional" class="w-50"></b-form-input>
         </b-form-group>
-        -->
+        <b-form-group label="" label-for="addnewaddress-submit" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+          <!-- <b-button size="sm" :disabled="!!addNewAddressFeedback" id="addnewaddress-submit" @click="addNewAddress" variant="primary">Add/Update</b-button> -->
+          <b-button size="sm" id="addnewaddress-submit" @click="addNewAddress" variant="primary">Add/Update</b-button>
+        </b-form-group>
       </b-modal>
     </div>
   `,
@@ -136,25 +93,27 @@ const NewAddress = {
         store.dispatch('newAddress/setAddress', address);
       },
     },
+    linkedToAddress: {
+      get: function () {
+        return store.getters['newAddress/linkedToAddress'];
+      },
+      set: function (linkedToAddress) {
+        store.dispatch('newAddress/setLinkedToAddress', linkedToAddress);
+      },
+    },
     phrase: {
       get: function () {
         return store.getters['newAddress/phrase'];
       },
       set: function (phrase) {
-        // store.dispatch('data/setAddressField', { account: store.getters['newAddress/address'], field: 'phrase', value: phrase });
         store.dispatch('newAddress/setPhrase', phrase);
       },
-    },
-
-    type() {
-      return store.getters['newAddress/type'];
     },
     mine: {
       get: function () {
         return store.getters['newAddress/mine'];
       },
       set: function (mine) {
-        store.dispatch('data/setAddressField', { account: store.getters['newAddress/address'], field: 'mine', value: mine });
         store.dispatch('newAddress/setMine', mine);
       },
     },
@@ -163,7 +122,6 @@ const NewAddress = {
         return store.getters['newAddress/favourite'];
       },
       set: function (favourite) {
-        store.dispatch('data/setAddressField', { account: store.getters['newAddress/address'], field: 'favourite', value: favourite });
         store.dispatch('newAddress/setFavourite', favourite);
       },
     },
@@ -172,7 +130,6 @@ const NewAddress = {
         return store.getters['newAddress/name'];
       },
       set: function (name) {
-        store.dispatch('data/setAddressField', { account: store.getters['newAddress/address'], field: 'name', value: name });
         store.dispatch('newAddress/setName', name);
       },
     },
@@ -181,12 +138,8 @@ const NewAddress = {
         return store.getters['newAddress/notes'];
       },
       set: function (notes) {
-        store.dispatch('data/setAddressField', { account: store.getters['newAddress/address'], field: 'notes', value: notes });
         store.dispatch('newAddress/setNotes', notes);
       },
-    },
-    source() {
-      return store.getters['newAddress/source'];
     },
     show: {
       get: function () {
@@ -211,9 +164,10 @@ const NewAddress = {
       logInfo("NewAddress", "methods.generateNewStealthMetaAddress - coinbase: " + this.coinbase);
       const phraseInHex = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(this.phrase));
       logInfo("NewAddress", "methods.generateNewStealthMetaAddress - phraseInHex: " + phraseInHex);
+      const linkedToAddress = this.coinbase;
       const signature = await ethereum.request({
         method: 'personal_sign',
-        params: [phraseInHex, this.coinbase],
+        params: [phraseInHex, linkedToAddress],
       });
       const signature1 = signature.slice(2, 66);
       const signature2 = signature.slice(66, 130);
@@ -230,14 +184,10 @@ const NewAddress = {
       const spendingPublicKey = ethers.utils.computePublicKey(keyPair1.privateKey, true);
       const viewingPublicKey = ethers.utils.computePublicKey(keyPair2.privateKey, true);
       const stealthMetaAddress = "st:eth:" + spendingPublicKey + viewingPublicKey.substring(2);
-      store.dispatch('newAddress/setKeys', { viewingPrivateKey, spendingPublicKey, viewingPublicKey, stealthMetaAddress });
-
-      // console.log("stealthMetaAddress: " + stealthMetaAddress);
-      // this.spendingPublicKey = spendingPublicKey;
-      // this.newAccount.viewingPublicKey = viewingPublicKey;
-      // this.newAccount.linkedToAddress = this.coinbase;
-      // this.newAccount.stealthMetaAddress = stealthMetaAddress;
-      // logInfo("NewAddress", "methods.generateNewStealthMetaAddress END: " + JSON.stringify(this.settings.newAccount, null, 2));
+      store.dispatch('newAddress/setKeys', { viewingPrivateKey, spendingPublicKey, viewingPublicKey, stealthMetaAddress, linkedToAddress });
+    },
+    async addNewAddress() {
+      logInfo("NewAddress", "methods.addNewAddress BEGIN");
     },
 
     async deleteAddress(account) {
@@ -347,13 +297,17 @@ const newAddressModule = {
       logInfo("newAddressModule", "mutations.setPhrase - phrase: " + phrase);
       state.phrase = phrase;
     },
+    setLinkedToAddress(state, linkedToAddress) {
+      logInfo("newAddressModule", "mutations.setLinkedToAddress - linkedToAddress: " + linkedToAddress);
+      state.linkedToAddress = linkedToAddress;
+    },
     setKeys(state, keys) {
       logInfo("newAddressModule", "mutations.setKeys - keys: " + JSON.stringify(keys, null, 2));
       state.address = keys.stealthMetaAddress;
       state.viewingPrivateKey = keys.viewingPrivateKey;
       state.spendingPublicKey = keys.spendingPublicKey;
       state.viewingPublicKey = keys.viewingPublicKey;
-      // state.linkedToAddress = this.coinbase;
+      state.linkedToAddress = keys.linkedToAddress;
     },
 
     setMine(state, mine) {
@@ -386,15 +340,19 @@ const newAddressModule = {
       await context.commit('setAction', action);
     },
     async setAddress(context, address) {
-      logInfo("newAddressModule", "addresss.setAddress - address: " + address);
+      logInfo("newAddressModule", "actions.setAddress - address: " + address);
       await context.commit('setAddress', address);
     },
     async setPhrase(context, phrase) {
-      logInfo("newAddressModule", "phrases.setPhrase - phrase: " + phrase);
+      logInfo("newAddressModule", "actions.setPhrase - phrase: " + phrase);
       await context.commit('setPhrase', phrase);
     },
+    async setLinkedToAddress(context, linkedToAddress) {
+      logInfo("newAddressModule", "actions.setLinkedToAddress - linkedToAddress: " + linkedToAddress);
+      await context.commit('setLinkedToAddress', linkedToAddress);
+    },
     async setKeys(context, keys) {
-      logInfo("newAddressModule", "keyss.setKeys - keys: " + JSON.stringify(keys, null, 2));
+      logInfo("newAddressModule", "actions.setKeys - keys: " + JSON.stringify(keys, null, 2));
       await context.commit('setKeys', keys);
     },
 
