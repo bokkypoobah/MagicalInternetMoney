ad84c7677212d2768b503c3e72c4cdbd9b3b5166
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-01-31 13:54:30 +1100

    wip

--- new_docs/data.js
+++ new_docs/data.js
@@ -1578,7 +1578,7 @@ const dataModule = {
       // IPFS retrieval failure        0xbe9371326F91345777b04394448c23E2BFEaa826 OSP Gemesis
 
       for (const [address, data] of Object.entries(context.state.tokenContracts[parameter.chainId] || {})) {
-        if (data.type == "erc721" && address == "0xbe9371326F91345777b04394448c23E2BFEaa826") {
+        if (data.type == "erc721") {
           console.log(address + " => " + JSON.stringify(data, null, 2));
           for (const [tokenId, tokenData] of Object.entries(data.tokenIds)) {
             console.log(address + "/" + tokenId + " => " + JSON.stringify(tokenData, null, 2));
