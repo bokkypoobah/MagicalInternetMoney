978bb4aa3c933101608ab9dae4e519317625fdbc
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-02-06 09:23:24 +1100

    forceRefresh

--- new_docs/data.js
+++ new_docs/data.js
@@ -1440,6 +1440,7 @@ const dataModule = {
               }
               completed++;
               if (completed % 10 == 0) {
+                context.commit('forceRefresh');
                 await context.dispatch('saveData', ['tokenContracts']);
               }
               context.commit('setSyncCompleted', completed);
